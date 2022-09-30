efcf9d8093da8dfe7c32ffaba9c21cdf875bf16b
emilinoooooo <108369658+emilinoooooo@users.noreply.github.com>
2022-09-30 16:58:42 -0500

    Add files via upload

---
+++ rrtcvb.js
@@ -0,0 +1,47 @@
+Webcam.set({widht:300,
+    height:300,
+    image_format:'png',png_quality:90
+
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){Webcam.snap(function(data_uri){document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
+
+    
+}
+
+
+)
+
+
+
+
+
+}
+console.log('ml5 version:',ml5.version);
+
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/fnkBmxx7j/model.json',modelLoaded);
+function modelLoaded(){
+classifier.classify(img, gotResult);
+
+}
+function check() { img = document.getElementById('selfie_image'); classifier.classify(img, gotResult); }
+
+
+
+function gotResult(error, result){
+    if (error) {
+        console.error(error);
+      } else {
+        // Los resultados están en un array ordenado por presición.
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      }
+}
+
+
+
+
+
+
